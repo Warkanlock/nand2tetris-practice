a9b0d2679e5736efd378cf28585454320c6c2207
Warkanlock <warkanlock@gmail.com>
2023-10-28 11:55:11 +0200

    chapter 4: fill.asm

--- materials/projects/04/fill/Fill.asm
+++ materials/projects/04/fill/Fill.asm
@@ -3,12 +3,48 @@
 // by Nisan and Schocken, MIT Press.
 // File name: projects/04/Fill.asm
 
-// Runs an infinite loop that listens to the keyboard input.
-// When a key is pressed (any key), the program blackens the screen,
-// i.e. writes "black" in every pixel;
-// the screen should remain fully black as long as the key is pressed. 
-// When no key is pressed, the program clears the screen, i.e. writes
-// "white" in every pixel;
-// the screen should remain fully clear as long as no key is pressed.
+// when key is pressed, screen should be black
+// when no key is pressed, screen should be white
 
-// Put your code here.
+(INIT_VAR)
+	@8192	
+	D=A
+
+	// set initialize to 8192 (32 * 256 pixels)
+	@i 
+	M=D
+
+(INIT_LOOP)
+	@i
+	M=M-1
+	D=M
+
+	@INIT_VAR
+	D;JLT // (index < 0, reset)
+
+	@KBD
+	D=M
+
+	@PIXEL_BLACK
+	D;JEQ
+
+	@PIXEL_WHITE
+	D;JMP
+
+(PIXEL_BLACK)             
+	@SCREEN
+	D=A
+	@i
+	A=D+M
+	M=-1  
+	@INIT_LOOP  
+	0;JMP
+
+(PIXEL_WHITE)
+	@SCREEN
+	D=A                
+	@i    
+	A=D+M 
+	M=0    
+	@INIT_LOOP  
+	0;JMP
