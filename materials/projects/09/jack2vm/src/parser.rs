cef5943e65ec7bd8227ee1eee2db80dbd0354c51
Warkanlock <warkanlock@gmail.com>
2024-09-08 18:33:55 +0200

    chore(jack2vm): add boundaries

--- materials/projects/09/jack2vm/src/parser.rs
+++ materials/projects/09/jack2vm/src/parser.rs
@@ -44,8 +44,10 @@ impl JackParser {
     }
 
     fn advance_token(&mut self) {
-        if self.current_token < self.tokens.len() {
+        if self.current_token <= self.tokens.len() {
             self.current_token += 1;
+        } else {
+            panic!("End of tokens reached. Cannot advance further.");
         }
     }
 
@@ -78,6 +80,7 @@ mod tests {
 
     #[test]
     fn test_parser_advance_token() {
+        // let a = 1;
         let tokens = vec![
             JackToken { token_type: JackTokenType::KEYWORD, keyword: Some(JackKeyword::LET), symbol: None, identifier: None, int_val: None, string_val: None },
             JackToken { token_type: JackTokenType::IDENTIFIER, keyword: None, symbol: None, identifier: Some("a".to_string()), int_val: None, string_val: None },
@@ -91,18 +94,38 @@ mod tests {
         assert_eq!(parser.current_token, 1);
     }
 
+    #[should_panic(expected = "End of tokens reached. Cannot advance further.")]
+    #[test]
+    fn test_parser_advance_token_until_overflow() {
+        // a++;
+        let tokens = vec![
+            JackToken { token_type: JackTokenType::IDENTIFIER, keyword: None, symbol: None, identifier: Some("a".to_string()), int_val: None, string_val: None },
+            JackToken { token_type: JackTokenType::SYMBOL, keyword: None, symbol: Some("+".to_string()), identifier: None, int_val: None, string_val: None },
+            JackToken { token_type: JackTokenType::SYMBOL, keyword: None, symbol: Some("+".to_string()), identifier: None, int_val: None, string_val: None }
+        ];
+
+        let mut parser = JackParser::new(tokens);
+        parser.advance_token(); // a
+        parser.advance_token(); // +
+        parser.advance_token(); // +
+        parser.advance_token(); // ;
+        parser.advance_token(); // should panic
+    }
+
     #[test]
     fn test_parser_get_current_token() {
+        // 1 + 1;
         let tokens = vec![
-            JackToken { token_type: JackTokenType::KEYWORD, keyword: Some(JackKeyword::LET), symbol: None, identifier: None, int_val: None, string_val: None },
-            JackToken { token_type: JackTokenType::IDENTIFIER, keyword: None, symbol: None, identifier: Some("a".to_string()), int_val: None, string_val: None },
-            JackToken { token_type: JackTokenType::SYMBOL, keyword: None, symbol: Some("=".to_string()), identifier: None, int_val: None, string_val: None },
+            JackToken { token_type: JackTokenType::INTCONST, keyword: None, symbol: None, identifier: None, int_val: Some(1), string_val: None },
+            JackToken { token_type: JackTokenType::SYMBOL, keyword: None, symbol: Some("+".to_string()), identifier: None, int_val: None, string_val: None },
             JackToken { token_type: JackTokenType::INTCONST, keyword: None, symbol: None, identifier: None, int_val: Some(1), string_val: None },
             JackToken { token_type: JackTokenType::SYMBOL, keyword: None, symbol: Some(";".to_string()), identifier: None, int_val: None, string_val: None }
         ];
 
-        let parser = JackParser::new(tokens);
-        let current_token = parser.get_current_token();
-        assert_eq!(current_token.token_type, JackTokenType::KEYWORD);
+        let mut parser = JackParser::new(tokens);
+        assert_eq!(parser.get_current_token().token_type, JackTokenType::INTCONST);
+        // advance to next token
+        parser.advance_token();
+        assert_eq!(parser.get_current_token().token_type, JackTokenType::SYMBOL);
     }
 }
